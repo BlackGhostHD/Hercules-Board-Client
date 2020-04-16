6798c193c4ba9a4c86b4648ed9cf50d609edf2bc
Christian <blackskill90@gmail.com>
2020-04-16 23:32:32 +0200

    added materialize-css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "materialize-css/dist/css/materialize.min.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/pages/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <a class="waves-effect waves-light btn">button</a>
+  </div>
+</template>
