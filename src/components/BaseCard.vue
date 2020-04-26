588b8ca87fc97e250e57d2e818777d81b82a3aef
Christian <blackskill90@gmail.com>
2020-04-26 17:34:59 +0200

    new BaseCard component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <section :class="classes">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+  props: {
+    type: {
+      type: String,
+      enum: ["default", "info", "warning", "danger"],
+      default: "dafault"
+    },
+    filled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classes() {
+      return ["card", this.type, this.filled ? "filled" : ""];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: var(--color-sidebar);
+  border-width: 2px;
+  border-style: solid;
+  border-color: var(--color-sidebar);
+
+  &.info {
+    border-color: var(--color-info);
+    &.filled {
+      color: var(--color-info-text);
+      background-color: var(--color-info);
+    }
+  }
+
+  &.warning {
+    border-color: var(--color-warning);
+    &.filled {
+      color: var(--color-warning-text);
+      background-color: var(--color-warning);
+    }
+  }
+
+  &.danger {
+    border-color: var(--color-danger);
+    &.filled {
+      color: var(--color-danger-text);
+      background-color: var(--color-danger);
+    }
+  }
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,5 +2,42 @@
   <div class="about">
     <a class="waves-effect waves-light btn">button</a>
     {{ $t("routes.name.dashboard") }}
+
+    <div class="row">
+      <div class="col s12">Cards</div>
+      <div class="col s12 m6 l4">
+        <BaseCard>default</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="info">info</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="warning">warning</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="danger">danger</BaseCard>
+      </div>
+      <div class="col s12">Cards filled</div>
+      <div class="col s12 m6 l4">
+        <BaseCard>default</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="info" :filled="true">info</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="warning" :filled="true">warning</BaseCard>
+      </div>
+      <div class="col s12 m6 l4">
+        <BaseCard type="danger" :filled="true">danger</BaseCard>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import BaseCard from "@/components/BaseCard";
+export default {
+  name: "About",
+  components: { BaseCard }
+};
+</script>
